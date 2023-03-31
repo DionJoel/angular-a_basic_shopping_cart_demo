7408d7e5d7985fc406abe1e9d1765b0efe7f20c8
DionJoel <68860957+DionJoel@users.noreply.github.com>
2023-03-31 13:20:24 +0200

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// Dieser Dienst bietet praktische Methoden zum Generieren von Steuerelementen.
+import { FormBuilder } from '@angular/forms';
 // Importieren Sie in cart.component.ts den CartService aus der Datei cart.service.ts.
 import { CartService } from '../cart.service';
 
@@ -15,8 +17,31 @@ export class CartComponent {
   */
   items = this.cartService.getItems();
 
+  /**
+   * Um den Namen und die Adresse des Benutzers zu erfassen, verwenden Sie die Methode FormBuilder group(), um die checkoutForm-Eigenschaft auf ein Formularmodell festzulegen, das Namens- und Adressfelder enthält.
+   * src/app/cart/cart.component.ts
+   */
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
     // Fügen Sie den CartService ein, damit die CartComponent ihn verwenden kann, indem Sie ihn dem Konstruktor () hinzufügen.
-    private cartService: CartService
+    private cartService: CartService,
+    // Dieser Dienst ist Teil des ReactiveFormsModule-Moduls, das Sie bereits importiert haben.
+    private formBuilder: FormBuilder
   ) {}
+
+  /**
+   * Definieren Sie eine onSubmit()-Methode, um das Formular zu verarbeiten.
+   * Bei dieser Methode können Benutzer ihren Namen und ihre Adresse übermitteln.
+   * Außerdem verwendet diese Methode die Methode clearCart() des CartService, um das Formular zurückzusetzen und den Warenkorb zu leeren.
+   */
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
